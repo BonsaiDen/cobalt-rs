2ace6e8f844358382698c735779590aaf3d6e8b4
Ivo Wetzel <ivo.wetzel@googlemail.com>
2016-01-14 18:13:52 +0100

    Switch to MIT/Apache 2.0 dual license.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2015-2016 Ivo Wetzel
+
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! **cobalt** is a networking library which provides [virtual connections
 //! over UDP](http://gafferongames.com/networking-for-game-programmers/udp-vs-tcp/)
 //! along with a messaging layer for sending both unreliable, reliable as well
--- src/shared/stats.rs
+++ src/shared/stats.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2015-2016 Ivo Wetzel
+
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 /// A structure containing stats data average of the course of one second.
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Stats {
