1e3e8904f35f2a4f0613957228c200705874fa5e
Ivo Wetzel <ivo.wetzel@googlemail.com>
2016-05-27 01:36:25 +0200

    [TEST] Remove unused imports.

--- src/tests/connection.rs
+++ src/tests/connection.rs
@@ -6,7 +6,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 use std::net;
-use std::iter;
 use std::thread;
 use std::time::Duration;
 
@@ -16,8 +15,7 @@ use super::super::{
     Connection,
     ConnectionState,
     Config,
-    MessageKind,
-    Handler
+    MessageKind
 };
 
 #[test]
