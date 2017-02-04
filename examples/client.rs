ffd1d33ec96f5b1dd3d8dd20602bde435bbb4dbc
Ivo Wetzel <ivo.wetzel@googlemail.com>
2017-02-04 01:24:47 +0100

    [EXAMPLE] Add basic client and server examples.

---
+++ examples/client.rs
@@ -0,0 +1,82 @@
+// Crates ---------------------------------------------------------------------
+extern crate cobalt;
+
+
+// Dependencies ---------------------------------------------------------------
+use cobalt::{
+    BinaryRateLimiter, Config, NoopPacketModifier, MessageKind, UdpSocket,
+    Client, ClientEvent
+};
+
+fn main() {
+
+    // Create a new client that communicates over a udp socket
+    let mut client = Client::<
+        UdpSocket,
+        BinaryRateLimiter,
+        NoopPacketModifier
+
+    >::new(Config::default());
+
+    // Make the client connect to port `1234` on `localhost`
+    println!("[Client] Connecting...");
+    client.connect("127.0.0.1:1234").expect("Failed to bind to socket.");
+
+    'main: loop {
+
+        // Accept incoming connections and fetch their events
+        while let Ok(event) = client.receive() {
+            // Handle events (e.g. Connection, Messages, etc.)
+            match event {
+                ClientEvent::Connection => {
+                    let conn = client.connection().unwrap();
+                    println!(
+                        "[Client] Connection established ({}, {}ms rtt).",
+                        conn.peer_addr(),
+                        conn.rtt()
+                    );
+
+                },
+                ClientEvent::Message(message) => {
+                    let conn = client.connection().unwrap();
+                    println!(
+                        "[Client] Message from server ({}, {}ms rtt): {:?}",
+                        conn.peer_addr(),
+                        conn.rtt(),
+                        message
+                    );
+
+                },
+                ClientEvent::ConnectionClosed(_) | ClientEvent::ConnectionLost => {
+                    let conn = client.connection().unwrap();
+                    println!(
+                        "[Client] ({}, {}ms rtt) disconnected.",
+                        conn.peer_addr(),
+                        conn.rtt()
+                    );
+                    break 'main;
+                },
+                _ => {}
+            }
+        }
+
+        // Send a message to all connected clients
+        if let Ok(conn) = client.connection() {
+            conn.send(MessageKind::Instant, b"Hello from Client".to_vec());
+        }
+
+        // Send all outgoing messages.
+        //
+        // Also auto delay the current thread to achieve the configured tick rate.
+        client.send(true).is_ok();
+
+    }
+
+    println!("[Client] Disconnecting...");
+
+    // Shutdown the server (freeing its socket and closing all its connections)
+    client.disconnect().ok();
+
+}
+
+
---
+++ examples/server.rs
@@ -0,0 +1,84 @@
+// Crates ---------------------------------------------------------------------
+extern crate cobalt;
+
+
+// Dependencies ---------------------------------------------------------------
+use cobalt::{
+    BinaryRateLimiter, Config, NoopPacketModifier, MessageKind, UdpSocket,
+    Server, ServerEvent
+};
+
+fn main() {
+
+    // Create a new server that communicates over a udp socket
+    let mut server = Server::<
+        UdpSocket,
+        BinaryRateLimiter,
+        NoopPacketModifier
+
+    >::new(Config::default());
+
+    // Make the server listen on port `1234` on all interfaces.
+    println!("[Server] Listening...");
+    server.listen("0.0.0.0:1234").expect("Failed to bind to socket.");
+
+    'main: loop {
+
+        // Accept incoming connections and fetch their events
+        while let Ok(event) = server.accept_receive() {
+            // Handle events (e.g. Connection, Messages, etc.)
+            match event {
+                ServerEvent::Connection(id) => {
+                    let conn = server.connection(&id).unwrap();
+                    println!(
+                        "[Server] Client {} ({}, {}ms rtt) connected.",
+                        id.0,
+                        conn.peer_addr(),
+                        conn.rtt()
+                    );
+
+                },
+                ServerEvent::Message(id, message) => {
+                    let conn = server.connection(&id).unwrap();
+                    println!(
+                        "[Server] Message from client {} ({}, {}ms rtt): {:?}",
+                        id.0,
+                        conn.peer_addr(),
+                        conn.rtt(),
+                        message
+                    );
+
+                },
+                ServerEvent::ConnectionClosed(id, _) | ServerEvent::ConnectionLost(id) => {
+                    let conn = server.connection(&id).unwrap();
+                    println!(
+                        "[Server] Client {} ({}, {}ms rtt) disconnected.",
+                        id.0,
+                        conn.peer_addr(),
+                        conn.rtt()
+                    );
+                    break 'main;
+                },
+                _ => {}
+            }
+        }
+
+        // Send a message to all connected clients
+        for (_, conn) in server.connections() {
+            conn.send(MessageKind::Instant, b"Hello from Server".to_vec());
+        }
+
+        // Send all outgoing messages.
+        //
+        // Also auto delay the current thread to achieve the configured tick rate.
+        server.send(true).is_ok();
+
+    }
+
+    println!("[Server] Shutting down...");
+
+    // Shutdown the server (freeing its socket and closing all its connections)
+    server.shutdown().ok();
+
+}
+
